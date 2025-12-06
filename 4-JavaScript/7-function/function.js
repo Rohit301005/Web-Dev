866d8aac41e99a573e3dca185ff66cafdcde64ca
Rohit301005 <rohitkumarrrawat04@gmail.com>
2025-12-06 20:38:33 +0530

    function

---
+++ 4-JavaScript/7-function/function.js
@@ -0,0 +1,15 @@
+// syntax
+
+// function name(parameter if required){
+//     staement
+// }
+
+
+function OnetoN(n){
+    for(let i = 1; i <=n ; i++){
+        console.log(i)
+    }
+}
+
+OnetoN(100)
+// OnetoN(10000000000000000)
---
+++ 4-JavaScript/7-function/mathsFunction.js
@@ -0,0 +1,11 @@
+// console.log(Math.abs(-7))
+// console.log(Math.max(5,1))
+// console.log(Math.max(5,1,30,90,399,23))
+// console.log(Math.min(5,1,30,90,399,23))
+
+// console.log(Math.pow(2,3))
+
+
+// math.random generates a real number b/w 0 to 1
+
+console.log(Math.random())
